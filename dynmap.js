6f790b16ee3c76b7875a88d0dcc2d186eac59ad3
AndrejNagy <46596011+AndrejNagy@users.noreply.github.com>
2023-09-06 10:47:37 +0200

    fix resize

--- dynmap.js
+++ dynmap.js
@@ -3,3 +3,5 @@ window.addEventListener('resize', function(event){
     document.getElementById('dynmap_iframe').style.height = window.innerHeight * 0.7 + 'px';
 }
 );
+//update height of iframe with id dynmap_iframe to the height of the window
+document.getElementById('dynmap_iframe').style.height = window.innerHeight * 0.7 + 'px';
